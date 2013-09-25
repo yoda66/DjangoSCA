54ddaecd772f71fddfeeeecd451f07288183963a
jsthyer <jsthyer@gmail.com>
2013-09-25 12:59:46 -0400

    import routine parser improvements

--- MyParser.py
+++ MyParser.py
@@ -13,7 +13,9 @@ class MyParser(ast.NodeVisitor):
     self.classes = []
     self.functions = []
     self.warnings = []
-    self.rxp = re.compile(r'<(.+) object at (.+)>')
+    self.obj_load = {}
+    self.obj_store = {}
+    self.rxpobj = re.compile(r'<(.+) object at (.+)>')
 
     # dictionaries for rules checking
     self.b_imports = {}
@@ -31,6 +33,7 @@ class MyParser(ast.NodeVisitor):
     node = ast.parse(code)
     self.shortname = shortname
     self.visit(node)
+    self.__django_clean_validator_check()
 
 
   def nonast_parse(self,projdir,shortname,code):
@@ -51,6 +54,15 @@ class MyParser(ast.NodeVisitor):
       print '%s' % (w)
 
 
+  def __django_clean_validator_check(self):
+    return
+    for l in self.obj_store:
+      print 'clean_val(): %s (%s)' % (l,self.obj_store[l])
+#      print 'store(): '+clean
+#    for f in self.functions:
+#      print 'func(): '+f
+
+
   def __load_rules(self,rulesfile):
     try:
       f = open(rulesfile, 'r')
@@ -68,26 +80,23 @@ class MyParser(ast.NodeVisitor):
       elif row[0] == 'template':
         self.b_template[row[1]] = row[2]
 
+    try: self.b_imports_re = self.__regex_compile(self.b_imports)
+    except: raise
+    try: self.b_strings_re = self.__regex_compile(self.b_strings)
+    except: raise
+    try: self.b_general_re = self.__regex_compile(self.b_general)
+    except: raise
+    try: self.b_template_re = self.__regex_compile(self.b_template)
+    except: raise
+
+
+  def __regex_compile(self,rule_dict):
     try:
-      self.b_imports_re = [re.compile(r) for r in self.b_imports]
+      re_dict = [re.compile(r) for r in rule_dict]
     except re.error as e:
       print '__load_rules(): regex compiled failed for [%s] [%s]' % (r,e)
       raise
-    try:
-      self.b_strings_re = [re.compile(r) for r in self.b_strings]
-    except re.error as e:
-      print '__load_rules(): regex compiled failed for [%s] [%s]' % (r,e)
-      raise
-    try:
-      self.b_general_re = [re.compile(r) for r in self.b_general]
-    except re.error as e:
-      print '__load_rules(): regex compiled failed for [%s] [%s]' % (r,e)
-      raise
-    try:
-      self.b_template_re = [re.compile(r) for r in self.b_template]
-    except re.error as e:
-      print '__load_rules(): regex compiled failed for [%s] [%s]' % (r,e)
-      raise
+    return re_dict
 
 
   def __crossdomain_xml(self,filename):
@@ -108,7 +117,6 @@ class MyParser(ast.NodeVisitor):
         self.warnings.append('L____: %s: Non-secure protocol access for domain [%s]' % (self.shortname,access.attrib['domain']))
 
 
-
   def __rxp_ast_check(self,mstr,node,sset,rset):
     for (p,v) in zip(rset,sset):
       try:
@@ -123,9 +131,11 @@ class MyParser(ast.NodeVisitor):
         print 'rxp_ast_check(): %s' % (re.error)
         raise
 
+
   def __istemplate(self):
     if len(self.__grep('{%|%}|{{|}}')) > 0: return True
     return False
+
 
   def __grep(self,exp):
     if not self.content: return []
@@ -138,6 +148,7 @@ class MyParser(ast.NodeVisitor):
       i += 1
     return mline
 
+
   def __rxp_nonast_check(self,sset,rset):
     for (p,v) in zip(rset,sset):
       for lineno in self.__grep(v):
@@ -145,52 +156,91 @@ class MyParser(ast.NodeVisitor):
 		(lineno,self.shortname,sset[v]))
         except: pass
 
+
   def visit_Import(self,node):
-    if self.debug: print 'visit_Import(): %s (%d)' % (node.names[0].name,node.lineno)
-    try: self.__rxp_ast_check(node.names[0].name,node,self.b_imports,self.b_imports_re)
-    except: pass
+    #if self.debug: print 'visit_Import(): %s (%d)' % (node.names[0].name,node.lineno)
+    for alias in node.names:
+      codeline = 'import '+getattr(alias,'name')
+      try: self.__rxp_ast_check(codeline,node,self.b_imports,self.b_imports_re)
+      except: pass
     self.generic_visit(node)
 
+
   def visit_ImportFrom(self,node):
-    if self.debug: print 'visit_ImportFrom(): %s' % (node.names[0])
-    try: self.__rxp_ast_check(node.names[0].name,node,self.b_imports,self.b_imports_re)
-    except: pass
+    #if self.debug: print 'visit_ImportFrom(): %s' % (node.names[0])
+    modulename = node.module
+    for alias in node.names:
+      codeline = 'from %s import %s' % (modulename,getattr(alias,'name'))
+      try: self.__rxp_ast_check(codeline,node,self.b_imports,self.b_imports_re)
+      except: pass
     self.generic_visit(node)
+
 
   def visit_ClassDef(self,node):
     self.classes.append('%s:%d,%d' % (node.name, node.lineno, node.col_offset))
     self.generic_visit(node)
 
+
   def visit_FunctionDef(self,node):
-    if self.debug: print 'visit_FunctionDef(): %s' % (node.name)
-    self.functions.append('%s:%d,%d' % (node.name, node.lineno, node.col_offset))
-    try: self.__rxp_ast_check(node.name,node,self.b_general,self.b_general_re)
-    except: pass
+    self.functions.append(node.name)
+    for statement in node.body: 
+      if self.rxpobj.match(str(statement)).group(1) == '_ast.Assign':
+        for target in statement.targets:
+          targetname = getattr(target,'id')
+          if self.rxpobj.match(str(statement.value)).group(1) == '_ast.Call':
+            rhs_func = getattr(getattr(statement.value,'func'),'value')
+            rhs_funcname = ''
+            if self.rxpobj.match(str(rhs_func)).group(1) == '_ast.Attribute':
+              rhs_funcname = getattr(rhs_func.value,'id')
+            else:
+              rhs_funcname = getattr(rhs_func,'id')
+
+            attr = self.__process_attr(getattr(statement.value,'func'))
+            if self.debug: print 'func(): (%s) %s = %s.%s' % \
+		(node.name,targetname,rhs_funcname,attr)
+#    try: self.__rxp_ast_check(node.name,node,self.b_general,self.b_general_re)
+#    except: pass
     self.generic_visit(node)
 
-#  def visit_Assign(self,node):
-#    print 'assign(): %s = %s' % \
-#	(str(node.targets[0]),str(node.value))
-#    self.generic_visit(node)
+
+  def __process_attr(self,node):
+    m = self.rxpobj.match(str(node))
+    retval = ''
+    if m.group(1) == '_ast.Name':
+      retval = getattr(node,'id')
+    elif m.group(1) == '_ast.Attribute':
+      retval += '#' + self.__process_attr(node.value)
+    if self.debug: print 'attr(): %s' % retval
+    return retval
 
   def visit_Str(self,node):
-    if self.debug: print 'visit_Str(): %s' % (str(node.s))
+    #if self.debug: print 'visit_Str(): %s' % (str(node.s))
     try: self.__rxp_ast_check(str(getattr(node,'s')),node,self.b_strings,self.b_strings_re)
     except: pass
     self.generic_visit(node)
 
-#  def visit_Tuple(self,node):
-#    for c in ast.iter_fields(node.elts):
-#      print 'tuple(): %s' % (str(c))
+
+#  def visit_Assign(self,node):
+#    m = self.rxpobj.match(str(node.value))
+#    if m.group(1) == '_ast.Call':
+#      lhs_node = getattr(node,'targets')[0]
+#      rhs_node = getattr(getattr(node.value,'func'),'value')
+#      temp = getattr(getattr(node.value,'func'),'ctx')
+#      lhs_m = self.rxpobj.match(str(lhs_node))
+#      rhs_m = self.rxpobj.match(str(rhs_node))
+#      if lhs_m.group(1) == '_ast.Name' and rhs_m.group(1) == '_ast.Name':
+#        print 'Assign(): %s (%s) = %s (%s) (%s)' % ( \
+#		getattr(lhs_node,'id'), lhs_m.group(2), \
+#		getattr(rhs_node,'id'), rhs_m.group(2), str(temp) )
 #    self.generic_visit(node)
 
-#  def visit_Name(self,node):
-#    for c in ast.iter_fields(node):
-#      print 'name(): %s' % (str(c))
-#    self.generic_visit(node)
 
-#  def visit_Call(self,node):
-#    for c in ast.iter_fields(node.func.value):
-#      print 'call(): %s' % (str(c))
-#    self.generic_visit(node)
+  def visit_Name(self,node):
+    m = self.rxpobj.match(str(getattr(node,'ctx')))
+    val = str(getattr(node,'id'))
+    if m.group(1) == '_ast.Store':
+      self.obj_store[val] = m.group(2)
+    elif m.group(1) == '_ast.Load':
+      self.obj_load[val] = m.group(2)
+    self.generic_visit(node)
 
