057904ef3afaaac121a8697925b0104513d392b3
Joff Thyer <jsthyer@thyer.org>
2014-06-19 17:12:44 -0400

    .

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from os.path import join, dirname
 
 setup(name='DjangoSCA',
-   version='1.3b',
+   version='1.3c',
    description='Django Static Source Code Analyzer',
    long_description = open(join(dirname(__file__), 'README.md')).read(),
    author='Joff Thyer',
