9dcd4869724cd8b903fc9518fc5287520b292b57
Joff Thyer <jsthyer@thyer.org>
2014-06-19 22:35:33 -0400

    .

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python
 
+import os
 from setuptools import setup, find_packages
-from os.path import join, dirname
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='DjangoSCA',
    version='1.3e',
    description='Django Static Source Code Analyzer',
-   long_description = open(join(dirname(__file__), 'README.md')).read(),
+   long_description = """
+DjangoSCA is a python based Django project source code security auditing system
+that makes use of the Django framework itself, the Python Abstract Syntax Tree
+(AST) library, and regular expressions.
+
+Django projects are laid out in a directory structure that conforms to a
+standard form using known classes, and standard file naming such as
+settings.py, urls.py, views.py, and forms.py.
+
+DjangoSCA is designed for the user to pass the root directory of the
+Django project as an argument to the program, from which it will
+recursively descend through the project files and perform source code
+checks on all python source code, and Django template files.
+""",
    author='Joff Thyer',
    author_email='jsthyer@gmail.com',
    license='GPLv3',
